743f96636bfc08b071b908cca1658deef1e259c1
Orlando Monterrosa <orlando.monterrosa95@gmail.com>
2021-08-12 23:23:49 -0600

    Correccion de errores
    Se cambio el tipo de datos en la funcion

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,11 +1,11 @@
 #5
 #Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 
-print("Ingrese dos numeros:")
+print("Ingrese dos numeros")
 
-a = input("Ingrese el primer numero: ")
-b = input("Ingrese el segundo numero: ")
+a = int(input("Ingrese el primer numero: "))
+b = int(input("Ingrese el segundo numero: "))
 
-print("El numero " + max(a,b) + " es mayor")
+print("El numero " + str(max(a,b)) + " es mayor")
     
 input("")
