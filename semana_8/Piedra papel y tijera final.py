977a63bb4651ab1247c2b454b0141e62916397fc
Orlando Monterrosa <orlando.monterrosa95@gmail.com>
2021-09-23 15:12:01 -0600

    Orlando Monterrosa
    Modificando la apariencia

--- semana_8/Piedra papel y tijera final.py
+++ semana_8/Piedra papel y tijera final.py
@@ -1,38 +1,45 @@
 import tkinter as tk
 import random
-from tkinter.constants import CENTER
-from typing import Text
 
-class Aplicacion:    
+#Creo la clase 
+class Aplicacion:
+
+#Defino el metodo constructor, titulo, tamaño, etc    
     def __init__(self):        
      self.ventana = tk.Tk() 
      self.ventana.title("Piedra papel y tijera")
-     self.ventana.geometry("200x200") 
+     self.ventana.geometry("250x200") 
 
-     self.label4 = tk.Label(self.ventana,text="Piedra",height=2)
-     self.label4.grid(column=0,row=0,padx = 5)
-     self.label4 = tk.Label(self.ventana,text="Papel",height=2)
-     self.label4.grid(column=0,row=1,padx = 5)
+#Creo los label y los posiciono
+     self.label2 = tk.Label(self.ventana,text="Piedra",height=2,padx=2)
+     self.label2.grid(column=0,row=0,padx=2)
+     self.label3 = tk.Label(self.ventana,text="Papel",height=2)
+     self.label3.grid(column=0,row=1)
      self.label4 = tk.Label(self.ventana,text="Tijera",height=2)
-     self.label4.grid(column=0,row=2,padx = 5)
+     self.label4.grid(column=0,row=2)
 
+#Almaceno el dato
      self.dato1 = tk.IntVar()
 
-     self.radiobuton1 = tk.Radiobutton(self.ventana,variable=self.dato1,value=1,width=15)
+#Creo los radiobutton
+     self.radiobuton1 = tk.Radiobutton(self.ventana,variable=self.dato1,value=1,width=18)
      self.radiobuton1.grid(column=1,row=0) 
      self.radiobuton2 = tk.Radiobutton(self.ventana,variable=self.dato1,value=2)
      self.radiobuton2.grid(column=1,row=1)
      self.radiobuton3 = tk.Radiobutton(self.ventana,variable=self.dato1,value=3)
      self.radiobuton3.grid(column=1,row=2)
 
-     self.result = tk.Label(self.ventana,text="Resultado",height=3,anchor="center")
+#Creo el label para almacenar el resultado
+     self.result = tk.Label(self.ventana,text="Resultado",height=3)
      self.result.grid(column=1,row=3)
-     self.boton1 = tk.Button(self.ventana, text="Jugar",command=self.jugar,anchor="center")
-     self.boton1.grid(column=1,row=7) 
+
+#Creo el boton
+     self.boton1 = tk.Button(self.ventana, text="Jugar",command=self.jugar)
+     self.boton1.grid(column=1,row=4) 
  
      self.ventana.mainloop()
 
-
+#Creo el metodo donde se elige un numero random entre 1 y 3, luego se evalua segun la selección del jugador
     def jugar(self):
      numr1=random.randint(1,3)
     
@@ -43,20 +50,17 @@ class Aplicacion:
      elif self.dato1.get() == 3 and numr1 == 3:
       self.result["text"]="Empate"
      elif self.dato1.get() == 1 and numr1 == 2:
-      self.result["text"]="Has perdido, sigue intentando"
+      self.result["text"]="Perdiste, sigue intentando"
      elif self.dato1.get() == 1 and numr1 == 3:
-      self.result["text"]="Felicidades has ganado"
+      self.result["text"]="Felicidades, has ganado"
      elif self.dato1.get() == 2 and numr1 == 1:
-      self.result["text"]="Felicidades has ganado"
+      self.result["text"]="Felicidades, has ganado"
      elif self.dato1.get() == 2 and numr1 == 3:
-      self.result["text"]="Has perdido, sigue intentando"
+      self.result["text"]="Perdiste, sigue intentando"
      elif self.dato1.get() == 3 and numr1 == 1:
-      self.result["text"]="Has perdido, sigue intentando"
+      self.result["text"]="Perdiste, sigue intentando"
      elif self.dato1.get() == 3 and numr1 == 2:
-      self.result["text"]="Felicidades has ganado"
-
-    def retrieve(self):
-     print(self.dato1.get())
+      self.result["text"]="Felicidades, has ganado"
 
 #Creando el objeto y llamando el programa
 aplicacion1 = Aplicacion()
