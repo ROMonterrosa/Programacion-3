74bdd027701a2d53fa7fdc9597e7fd813466667d
Orlando Monterrosa <orlando.monterrosa95@gmail.com>
2021-10-20 09:38:22 -0600

    Orlando Monterrosa
    Cambios en el proyecto semana 11-12 y parcial

--- Semana_11_12/Proyecto/app.py
+++ Semana_11_12/Proyecto/app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route("/",methods=["GET","¨POST"])
 def inicio():
@@ -17,5 +18,18 @@ def SanMiguel():
 def enlaces():
     return render_template("enlaces.html")
 
+@app.route('/resultado', methods=["GET",'POST'])
+def resultado():
+    respuesta=""
+    if request.method =="POST" and "num1" in request.form and "num2" in request.form:
+#    if request.method =="POST":
+        num1=request.form.get("num1")
+        num2=request.form.get("num2")
+        respuesta = num1
+        if num1>num2:
+                respuesta = num1
+        else:
+                respuesta = num2
+    return render_template("resultado.html",respuesta=respuesta)
 
 app.run(debug=True)
